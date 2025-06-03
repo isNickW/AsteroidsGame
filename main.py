e68e7d2fe41b2b5b9c6817e2018ca4f9fd011c33
nickwray <nickjwray@gmail.com>
2025-06-03 15:35:15 -0400

    add circlechape.py, updated main.py

--- main.py
+++ main.py
@@ -5,18 +5,23 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init()
     print(f"""Starting Asteroids!
     Screen width: {SCREEN_WIDTH}
     Screen height: {SCREEN_HEIGHT}
     """)
+
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
