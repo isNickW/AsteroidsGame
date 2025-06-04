76c3066daad2eaf4f94e0ff181e80b1085abd1c4
nickwray <nickjwray@gmail.com>
2025-06-04 13:23:13 -0400

    update main.py

--- main.py
+++ main.py
@@ -22,13 +22,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
         screen.fill("black")
         player.draw(screen)
-        
+        player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
-        
 
 if __name__ == "__main__":
     main()
