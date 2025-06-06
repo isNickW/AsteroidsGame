f945bcbc1a8827bcd7cec8f75f6f680b7a99a56e
nickwray <nickjwray@gmail.com>
2025-06-06 00:23:30 -0400

    added shot.py and implemented

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
 from player import Player
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
+from shot import Shot
 
 def main():
     print(f"""Starting Asteroids!
@@ -18,12 +20,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    # Groups #
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    # Containers #
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     asteroidField = AsteroidField()
     
@@ -32,10 +40,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         updatable.update(dt)
+
         for object in drawable:
             object.draw(screen)
+        for object in asteroids:
+            if object.collisioncheck(player):
+                print("Game Over!")
+                sys.exit()
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
